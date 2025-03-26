0ba47bf1e283dcd591a4cb8d6515289c71b80948
jiten0709 <parmar.jiten.microsoft@gmail.com>
2025-03-26 13:01:02 +0530

    publish app to git/github

---
+++ README.md
@@ -0,0 +1,70 @@
+# DERMALYSE APP'S API
+
+This API call is used to detect what top 3 most probable diseases a provided image is.
+Its a made with Flask framework...follow SETUP STPES to locally run it on your machine.
+
+## API Reference
+
+#### Its hosted on zeet.co
+
+```http
+  LINK
+```
+
+#### Send image as payload
+
+```http
+  POST /5001:predict/
+```
+
+```http
+  Response {'disease1': probability1,'disease2': probability2,'disease3': probability3}
+```
+
+| Parameter | Type             | Description                                             |
+| :-------- | :--------------- | :------------------------------------------------------ |
+| `file`    | `base64 encoded` | **Required**. an image file converted to base64 encoded |
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/jiten0709/dermalyseAPI.git
+```
+
+Go to the project directory
+
+```bash
+  cd dermalyseAPI
+```
+
+Create a VirtualEnv
+
+```bash
+python3.10 -m venv my_venv
+```
+
+Activate VirtualEnv
+
+```bash
+source my_venv/bin/activate
+```
+
+Install dependencies
+
+```bash
+  pip install -r requirements.txt
+```
+
+Start the server
+
+```bash
+  python3 main.py
+```
+
+## Connect with NGROK
+
+```bash
+  ngrok http http://127.0.0.1:5001
+```
---
+++ main.py
@@ -0,0 +1,121 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+import numpy as np
+from PIL import Image as image
+from io import BytesIO
+import base64
+# from tensorflow.keras.preprocessing import image as keras_image
+
+
+app = Flask(__name__)
+
+# Load your model
+model = tf.keras.models.load_model('new_xception_model.keras', compile=False)
+
+class_labels = {
+    'Acne': 0,
+    'Actinic keratosis': 1,
+    'Basal Cell Carcinoma': 2,
+    'Benign Keratosis': 3,
+    'Dermatofibroma': 4,
+    'Eczema': 5,
+    'Fungal Infection': 6,
+    'Melanocytic Nevi': 7,
+    'Melanoma': 8,
+    'Rosacea': 9,
+    'Squamous cell carcinoma': 10,
+    'Urticaria Hives': 11,
+    'Vascular Tumors': 12,
+    'Viral Infections': 13,
+    'Warts Molluscum': 14
+}
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    return jsonify({'message': 'hello'})
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    if 'image' not in data:
+        return jsonify({'error': 'No image data'}), 400
+
+    # Decode the Base64 string back into bytes
+    image_bytes = base64.b64decode(data['image'])
+    # Convert the bytes to a PIL Image object
+    img = image.open(BytesIO(image_bytes))
+    # Resize the image to the target size
+    img = img.resize((299, 299))
+    # Convert the image to a NumPy array
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+
+    img_array = np.expand_dims(img_array, axis=0) / 255.
+
+    # Assuming model is already loaded
+    predictions = model.predict(img_array)
+    instance_predictions = predictions[0]
+    flattened_predictions = instance_predictions.flatten()
+    sorted_indices = np.argsort(flattened_predictions)[::-1]
+
+    # Initialize an empty list to store the sorted predictions
+    sorted_predictions = []
+
+    for index in sorted_indices:
+        class_name = list(class_labels.keys())[index]
+        # Convert float32 to float, multiply by 100 to get percentage, and round to 2 decimal places
+        probability = round(float(flattened_predictions[index]) * 100, 2)
+        # Store the class name and probability as a tuple
+        if probability > 30:
+            sorted_predictions.append((class_name, probability))
+    
+    # Convert the list of tuples into a dictionary
+    top3_predictions = dict(sorted_predictions[:3])
+
+    print(top3_predictions)
+
+    return jsonify(top3_predictions)
+
+@app.route('/test', methods=['GET'])
+def test():
+    # Open the image file
+    with open('test_img.jpg', 'rb') as image_file:
+        # Convert the file stream to bytes
+        image_bytes = image_file.read()
+    
+    # Convert the bytes to a PIL Image object
+    img = image.open(BytesIO(image_bytes))
+    # Resize the image to the target size
+    img = img.resize((299, 299))
+    # Convert the image to a NumPy array
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+
+    img_array = np.expand_dims(img_array, axis=0) / 255.
+
+    # Assuming model is already loaded
+    predictions = model.predict(img_array)
+    instance_predictions = predictions[0]
+    flattened_predictions = instance_predictions.flatten()
+    sorted_indices = np.argsort(flattened_predictions)[::-1]
+
+    # Initialize an empty list to store the sorted predictions
+    sorted_predictions = []
+
+    for index in sorted_indices:
+        class_name = list(class_labels.keys())[index]
+        # Convert float32 to float, multiply by 100 to get percentage, and round to 2 decimal places
+        probability = round(float(flattened_predictions[index]) * 100, 2)
+        # Store the class name and probability as a tuple
+        if probability > 30:
+            sorted_predictions.append((class_name, probability))
+    
+    # Convert the list of tuples into a dictionary
+    top3_predictions = dict(sorted_predictions[:3])
+
+    print(top3_predictions)
+
+    return jsonify(top3_predictions)
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5001)
